482dd4574f9d8dc232d7af5ac212fd4d4153723c
Surya V <suryasv0729@gmail.com>
2024-06-16 22:03:58 +0530

    responsive added

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const hamburger = document.querySelector(".hamburger");
-const sidenav = document.querySelector(".side_nav");
-console.log(sidenav);
-hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("is-active");
-  sidenav.classList.toggle("open");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+document.addEventListener("DOMContentLoaded", function () {
+  const mobileMenu = document.querySelector(".mobile-menu");
+
+  hamburgerMenu.addEventListener("click", function () {
+    mobileMenu.style.display =
+      mobileMenu.style.display === "flex" ? "none" : "flex";
+  });
 });
